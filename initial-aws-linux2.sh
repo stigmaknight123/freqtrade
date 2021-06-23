54474df8305f28e9c83f271124957d436fbf7bf4
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2021-06-23 09:16:50 +0900

    Create initial-aws-linux2.sh

---
+++ initial-aws-linux2.sh
@@ -0,0 +1,7 @@
+screen -S freqtrade
+sudo yum update -y
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/initial_freqtrade.sh
+bash initial_freqtrade.sh
