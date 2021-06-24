3c9e6e44b71db09024a774b27f07ce970b321c7f
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2021-06-24 11:46:59 +0900

    Create SMAOffsetProtectOptV1_dynamic.py

---
+++ user_data/strategies/SMAOffsetProtectOptV1_dynamic.py
@@ -0,0 +1,301 @@
+# --- Do not remove these libs ---
+from freqtrade.strategy.interface import IStrategy
+from typing import Dict, List
+from functools import reduce
+from pandas import DataFrame
+# --------------------------------
+
+import talib.abstract as ta
+import numpy as np
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+import datetime
+from technical.util import resample_to_interval, resampled_merge
+from datetime import datetime, timedelta
+from freqtrade.persistence import Trade
+from freqtrade.strategy import stoploss_from_open, merge_informative_pair, DecimalParameter, IntParameter, CategoricalParameter
+import technical.indicators as ftt
+
+# Buy hyperspace params:
+buy_params = {
+    "base_nb_candles_buy": 16,
+    "ewo_high": 5.638,
+    "ewo_low": -19.993,
+    "low_offset": 0.978,
+    "rsi_buy": 61,
+}
+
+# Sell hyperspace params:
+sell_params = {
+    "base_nb_candles_sell": 49,
+    "high_offset": 1.006,
+}
+
+def EWO(dataframe, ema_length=5, ema2_length=35):
+    df = dataframe.copy()
+    ema1 = ta.EMA(df, timeperiod=ema_length)
+    ema2 = ta.EMA(df, timeperiod=ema2_length)
+    emadif = (ema1 - ema2) / df['close'] * 100
+    return emadif
+
+
+class SMAOffsetProtectOptV1_dynamic(IStrategy):
+    INTERFACE_VERSION = 2
+
+    # ROI table:
+    minimal_roi = {
+        "0": 0.01
+    }
+
+    # Stoploss:
+    stoploss = -0.5
+
+    # SMAOffset
+    base_nb_candles_buy = IntParameter(
+        5, 80, default=buy_params['base_nb_candles_buy'], space='buy', optimize=True)
+    base_nb_candles_sell = IntParameter(
+        5, 80, default=sell_params['base_nb_candles_sell'], space='sell', optimize=True)
+    low_offset = DecimalParameter(
+        0.9, 0.99, default=buy_params['low_offset'], space='buy', optimize=True)
+    high_offset = DecimalParameter(
+        0.99, 1.1, default=sell_params['high_offset'], space='sell', optimize=True)
+
+    # Protection
+    fast_ewo = 50
+    slow_ewo = 200
+    ewo_low = DecimalParameter(-20.0, -8.0,
+                               default=buy_params['ewo_low'], space='buy', optimize=True)
+    ewo_high = DecimalParameter(
+        2.0, 12.0, default=buy_params['ewo_high'], space='buy', optimize=True)
+    rsi_buy = IntParameter(30, 70, default=buy_params['rsi_buy'], space='buy', optimize=True)
+
+
+    # Trailing stop:
+    trailing_stop = False
+    trailing_stop_positive = 0.001
+    trailing_stop_positive_offset = 0.01
+    trailing_only_offset_is_reached = True
+
+    # Sell signal
+    use_sell_signal = True
+    sell_profit_only = False
+    sell_profit_offset = 0.01
+    ignore_roi_if_buy_signal = True
+
+    # Optimal timeframe for the strategy
+    timeframe = '5m'
+    informative_timeframe = '1h'
+
+    process_only_new_candles = True
+    startup_candle_count = 30
+
+    plot_config = {
+        'main_plot': {
+            'ma_buy': {'color': 'orange'},
+            'ma_sell': {'color': 'orange'},
+        },
+    }
+
+    use_custom_stoploss = True
+    custom_stop = {
+        # Linear Decay Parameters
+        'decay-time': 166,       # minutes to reach end, I find it works well to match this to the final ROI value - default 1080
+        'decay-delay': 0,         # minutes to wait before decay starts
+        'decay-start': -0.085, # -0.32118, # -0.07163,     # starting value: should be the same or smaller than initial stoploss - default -0.30
+        'decay-end': -0.02,       # ending value - default -0.03
+        # Profit and TA  
+        'cur-min-diff': 0.03,     # diff between current and minimum profit to move stoploss up to min profit point
+        'cur-threshold': -0.02,   # how far negative should current profit be before we consider moving it up based on cur/min or roc
+        'roc-bail': -0.03,        # value for roc to use for dynamic bailout
+        'rmi-trend': 50,          # rmi-slow value to pause stoploss decay
+        'bail-how': 'immediate',  # set the stoploss to the atr offset below current price, or immediate
+        # Positive Trailing
+        'pos-trail': True,        # enable trailing once positive  
+        'pos-threshold': 0.005,   # trail after how far positive
+        'pos-trail-dist': 0.015   # how far behind to place the trail
+    }
+
+    use_dynamic_roi = True
+    
+    # Dynamic ROI
+    droi_trend_type = CategoricalParameter(['rmi', 'ssl', 'candle', 'any'], default='any', space='sell', optimize=True)
+    droi_pullback = CategoricalParameter([True, False], default=True, space='sell', optimize=True)
+    droi_pullback_amount = DecimalParameter(0.005, 0.02, default=0.005, space='sell')
+    droi_pullback_respect_table = CategoricalParameter([True, False], default=False, space='sell', optimize=True)
+
+    def informative_pairs(self):
+
+        pairs = self.dp.current_whitelist()
+        informative_pairs = [(pair, self.informative_timeframe) for pair in pairs]
+
+        return informative_pairs
+
+    def get_informative_indicators(self, metadata: dict):
+
+        dataframe = self.dp.get_pair_dataframe(
+            pair=metadata['pair'], timeframe=self.informative_timeframe)
+
+        return dataframe
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        # Calculate all ma_buy values
+        for val in self.base_nb_candles_buy.range:
+            dataframe[f'ma_buy_{val}'] = ta.EMA(dataframe, timeperiod=val)
+
+        # Calculate all ma_sell values
+        for val in self.base_nb_candles_sell.range:
+            dataframe[f'ma_sell_{val}'] = ta.EMA(dataframe, timeperiod=val)
+
+        # Elliot
+        dataframe['EWO'] = EWO(dataframe, self.fast_ewo, self.slow_ewo)
+        
+        # RSI
+        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
+
+        # Slam some indicators into the trade_info dict so we can dynamic roi and custom stoploss in backtest
+        if self.dp.runmode.value in ('backtest', 'hyperopt'):
+            self.custom_trade_info[metadata['pair']]['roc'] = dataframe[['date', 'roc']].copy().set_index('date')
+            self.custom_trade_info[metadata['pair']]['atr'] = dataframe[['date', 'atr']].copy().set_index('date')
+            self.custom_trade_info[metadata['pair']]['sroc'] = dataframe[['date', 'sroc']].copy().set_index('date')
+            self.custom_trade_info[metadata['pair']]['ssl-dir'] = dataframe[['date', 'ssl-dir']].copy().set_index('date')
+            self.custom_trade_info[metadata['pair']]['rmi-up-trend'] = dataframe[['date', 'rmi-up-trend']].copy().set_index('date')
+            self.custom_trade_info[metadata['pair']]['candle-up-trend'] = dataframe[['date', 'candle-up-trend']].copy().set_index('date')
+
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        conditions = []
+
+        conditions.append(
+            (
+                (dataframe['close'] < (dataframe[f'ma_buy_{self.base_nb_candles_buy.value}'] * self.low_offset.value)) &
+                (dataframe['EWO'] > self.ewo_high.value) &
+                (dataframe['rsi'] < self.rsi_buy.value) &
+                (dataframe['volume'] > 0)
+            )
+        )
+
+        conditions.append(
+            (
+                (dataframe['close'] < (dataframe[f'ma_buy_{self.base_nb_candles_buy.value}'] * self.low_offset.value)) &
+                (dataframe['EWO'] < self.ewo_low.value) &
+                (dataframe['volume'] > 0)
+            )
+        )
+
+        if conditions:
+            dataframe.loc[
+                reduce(lambda x, y: x | y, conditions),
+                'buy'
+            ]=1
+
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        conditions = []
+
+        conditions.append(
+            (
+                (dataframe['close'] > (dataframe[f'ma_sell_{self.base_nb_candles_sell.value}'] * self.high_offset.value)) &
+                (dataframe['volume'] > 0)
+            )
+        )
+
+        if conditions:
+            dataframe.loc[
+                reduce(lambda x, y: x | y, conditions),
+                'sell'
+            ]=1
+
+        return dataframe
+        
+    """
+    Everything from here completely stolen from the godly work of @werkkrew
+    
+    Custom Stoploss 
+    """ 
+    def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime, current_rate: float, current_profit: float, **kwargs) -> float:
+        trade_dur = int((current_time.timestamp() - trade.open_date_utc.timestamp()) // 60)
+
+        if self.config['runmode'].value in ('live', 'dry_run'):
+            dataframe, last_updated = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
+            sroc = dataframe['sroc'].iat[-1]
+        # If in backtest or hyperopt, get the indicator values out of the trades dict (Thanks @JoeSchr!)
+        else:
+            sroc = self.custom_trade_info[trade.pair]['sroc'].loc[current_time]['sroc']
+
+        if current_profit < self.cstp_threshold.value:
+            if self.cstp_bail_how.value == 'roc' or self.cstp_bail_how.value == 'any':
+                # Dynamic bailout based on rate of change
+                if (sroc/100) <= self.cstp_bail_roc.value:
+                    return 0.001
+            if self.cstp_bail_how.value == 'time' or self.cstp_bail_how.value == 'any':
+                # Dynamic bailout based on time
+                if trade_dur > self.cstp_bail_time.value:
+                    return 0.001
+                   
+        return 1
+
+    """
+    Freqtrade ROI Overload for dynamic ROI functionality
+    """
+    def min_roi_reached_dynamic(self, trade: Trade, current_profit: float, current_time: datetime, trade_dur: int) -> Tuple[Optional[int], Optional[float]]:
+
+        minimal_roi = self.minimal_roi
+        _, table_roi = self.min_roi_reached_entry(trade_dur)
+
+        # see if we have the data we need to do this, otherwise fall back to the standard table
+        if self.custom_trade_info and trade and trade.pair in self.custom_trade_info:
+            if self.config['runmode'].value in ('live', 'dry_run'):
+                dataframe, last_updated = self.dp.get_analyzed_dataframe(pair=trade.pair, timeframe=self.timeframe)
+                rmi_trend = dataframe['rmi-up-trend'].iat[-1]
+                candle_trend = dataframe['candle-up-trend'].iat[-1]
+                ssl_dir = dataframe['ssl-dir'].iat[-1]
+            # If in backtest or hyperopt, get the indicator values out of the trades dict (Thanks @JoeSchr!)
+            else:
+                rmi_trend = self.custom_trade_info[trade.pair]['rmi-up-trend'].loc[current_time]['rmi-up-trend']
+                candle_trend = self.custom_trade_info[trade.pair]['candle-up-trend'].loc[current_time]['candle-up-trend']
+                ssl_dir = self.custom_trade_info[trade.pair]['ssl-dir'].loc[current_time]['ssl-dir']
+
+            min_roi = table_roi
+            max_profit = trade.calc_profit_ratio(trade.max_rate)
+            pullback_value = (max_profit - self.droi_pullback_amount.value)
+            in_trend = False
+
+            if self.droi_trend_type.value == 'rmi' or self.droi_trend_type.value == 'any':
+                if rmi_trend == 1:
+                    in_trend = True
+            if self.droi_trend_type.value == 'ssl' or self.droi_trend_type.value == 'any':
+                if ssl_dir == 'up':
+                    in_trend = True
+            if self.droi_trend_type.value == 'candle' or self.droi_trend_type.value == 'any':
+                if candle_trend == 1:
+                    in_trend = True
+
+            # Force the ROI value high if in trend
+            if (in_trend == True):
+                min_roi = 100
+                # If pullback is enabled, allow to sell if a pullback from peak has happened regardless of trend
+                if self.droi_pullback.value == True and (current_profit < pullback_value):
+                    if self.droi_pullback_respect_table.value == True:
+                        min_roi = table_roi
+                    else:
+                        min_roi = current_profit / 2
+
+        else:
+            min_roi = table_roi
+
+        return trade_dur, min_roi
+
+    # Change here to allow loading of the dynamic_roi settings
+    def min_roi_reached(self, trade: Trade, current_profit: float, current_time: datetime) -> bool:  
+        trade_dur = int((current_time.timestamp() - trade.open_date_utc.timestamp()) // 60)
+
+        if self.use_dynamic_roi:
+            _, roi = self.min_roi_reached_dynamic(trade, current_profit, current_time, trade_dur)
+        else:
+            _, roi = self.min_roi_reached_entry(trade_dur)
+        if roi is None:
+            return False
+        else:
+            return current_profit > roi   
