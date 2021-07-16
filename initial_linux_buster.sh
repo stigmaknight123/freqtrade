9e9e24d7620878a0beb814d8635acb8df3f332f1
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2021-07-16 09:43:32 +0900

    Update initial_linux_buster.sh

--- initial_linux_buster.sh
+++ initial_linux_buster.sh
@@ -1,4 +1,4 @@
-sudo apt-get -y install python3-venv libatlas-base-dev cmake
+sudo apt-get -y install python3-pip python3-venv libatlas-base-dev cmake git
 sudo echo "[global]\nextra-index-url=https://www.piwheels.org/simple" > tee /etc/pip.conf
 git clone https://github.com/stash86/freqtrade.git
 cd freqtrade
