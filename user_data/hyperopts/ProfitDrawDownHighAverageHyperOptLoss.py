b9a5944e17e681ad5c65417daccfb5a58d2aab29
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2021-08-08 08:49:25 +0900

    update

--- user_data/hyperopts/ProfitDrawDownHighAverageHyperOptLoss.py
+++ user_data/hyperopts/ProfitDrawDownHighAverageHyperOptLoss.py
@@ -10,6 +10,8 @@ class ProfitDrawDownHighAverageHyperOptLoss(IHyperOptLoss):
     @staticmethod
     def hyperopt_loss_function(results: DataFrame, trade_count: int, *args, **kwargs) -> float:
         total_profit = results['profit_abs'].sum()
+        total_profit_ratio = results['profit_ratio'].sum()
+        avg_profit = total_profit_ratio / trade_count
 
         # from freqtrade.optimize.optimize_reports.generate_strategy_stats()
         try:
@@ -20,4 +22,7 @@ class ProfitDrawDownHighAverageHyperOptLoss(IHyperOptLoss):
         if ((max_drawdown_per * DRAWDOWN_MULT) > 1) and (total_profit < 0):
             total_profit = total_profit * -1
 
-        return -1 * (total_profit * (1 - max_drawdown_per * DRAWDOWN_MULT) / trade_count)
+        if (avg_profit < 1) and (total_profit * (1 - max_drawdown_per * DRAWDOWN_MULT) < 0):
+            total_profit = total_profit * -1
+
+        return -1 * (total_profit * (1 - max_drawdown_per * DRAWDOWN_MULT) * (avg_profit - 1))
