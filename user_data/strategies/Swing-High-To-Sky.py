28f9dad9ebcc1e2d56d7286488ac02148ab2358c
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2021-08-20 19:56:14 +0900

    update

--- user_data/strategies/Swing-High-To-Sky.py
+++ user_data/strategies/Swing-High-To-Sky.py
@@ -42,15 +42,15 @@ class SwingHighToSky(IStrategy):
 
     minimal_roi = {"0": 0.27058, "33": 0.0853, "64": 0.04093, "244": 0}
 
-    buy_cci = IntParameter(low=-200, high=200, default=100, space='buy', optimize=True)
-    buy_cciTime = IntParameter(low=10, high=80, default=20, space='buy', optimize=True)
-    buy_rsi = IntParameter(low=10, high=90, default=30, space='buy', optimize=True)
-    buy_rsiTime = IntParameter(low=10, high=80, default=26, space='buy', optimize=True)
+    buy_cci = IntParameter(low=-200, high=200, default=100, space='buy', optimize=False)
+    buy_cciTime = IntParameter(low=10, high=80, default=20, space='buy', optimize=False)
+    buy_rsi = IntParameter(low=10, high=90, default=30, space='buy', optimize=False)
+    buy_rsiTime = IntParameter(low=10, high=80, default=26, space='buy', optimize=False)
 
-    sell_cci = IntParameter(low=-200, high=200, default=100, space='sell', optimize=True)
-    sell_cciTime = IntParameter(low=10, high=80, default=20, space='sell', optimize=True)
-    sell_rsi = IntParameter(low=10, high=90, default=30, space='sell', optimize=True)
-    sell_rsiTime = IntParameter(low=10, high=80, default=26, space='sell', optimize=True)
+    sell_cci = IntParameter(low=-200, high=200, default=100, space='sell', optimize=False)
+    sell_cciTime = IntParameter(low=10, high=80, default=20, space='sell', optimize=False)
+    sell_rsi = IntParameter(low=10, high=90, default=30, space='sell', optimize=False)
+    sell_rsiTime = IntParameter(low=10, high=80, default=26, space='sell', optimize=False)
 
     # Buy hyperspace params:
     buy_params = {
@@ -72,22 +72,12 @@ class SwingHighToSky(IStrategy):
         return []
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
-        for val in self.buy_cciTime.range:
-            dataframe[f'cci-{val}'] = ta.CCI(dataframe, timeperiod=val)
-
-        for val in self.sell_cciTime.range:
-            dataframe[f'cci-sell-{val}'] = ta.CCI(dataframe, timeperiod=val)
-
-        for val in self.buy_rsiTime.range:
-            dataframe[f'rsi-{val}'] = ta.RSI(dataframe, timeperiod=val)
-
-        for val in self.sell_rsiTime.range:
-            dataframe[f'rsi-sell-{val}'] = ta.RSI(dataframe, timeperiod=val)
-
         return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+        dataframe[f'cci-{self.buy_cciTime.value}'] = ta.CCI(dataframe, timeperiod=int(self.buy_cciTime.value))
+        dataframe[f'rsi-{self.buy_rsiTime.value}'] = ta.RSI(dataframe, timeperiod=int(self.buy_rsiTime.value))
 
         dataframe.loc[
             (
@@ -100,6 +90,8 @@ class SwingHighToSky(IStrategy):
 
     def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
+        dataframe[f'cci-sell-{self.sell_cciTime.value}'] = ta.CCI(dataframe, timeperiod=int(self.sell_cciTime.value))
+        dataframe[f'rsi-sell-{self.sell_rsiTime.value}'] = ta.RSI(dataframe, timeperiod=int(self.sell_rsiTime.value))
         dataframe.loc[
             (
                 (dataframe[f'cci-sell-{self.sell_cciTime.value}'] > self.sell_cci.value) &
