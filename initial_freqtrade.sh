3191944dfe63ed273acdac7a340f7d4b73904c49
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2021-08-28 11:20:14 +0900

    Update initial_freqtrade.sh

--- initial_freqtrade.sh
+++ initial_freqtrade.sh
@@ -24,24 +24,3 @@ if [ ! -d "$DIRECTORY" ]; then
   mkdir strategies
 fi
 cd strategies/
-sudo wget https://raw.githubusercontent.com/i1ya/freqtrade-strategies/main/BigZ03/BigZ03.py
-sudo wget https://raw.githubusercontent.com/i1ya/freqtrade-strategies/main/BigZ04/BigZ04.py
-sudo wget https://raw.githubusercontent.com/i1ya/freqtrade-strategies/main/BigZ06/BigZ06.py
-sudo wget https://raw.githubusercontent.com/i1ya/freqtrade-strategies/main/BigZ07/BigZ07.py
-sudo wget https://raw.githubusercontent.com/froggleston/cryptofrog-strategies/main/CryptoFrog.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/DevilStra.py
-sudo wget https://raw.githubusercontent.com/freqtrade/freqtrade-strategies/master/user_data/strategies/GodStra.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/GodStraNew.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/GodStraNewOpt.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/GodStraNewOptQuick.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/CombinedBinHAndClucV8.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/MultiOffsetLamboV0.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/NostalgiaForInfinityV3.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/NostalgiaForInfinityV4.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/NostalgiaForInfinityV5.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/NostalgiaForInfinityV5MultiOffsetAndHO.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/NormalizerStrategy.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/CombinedBinHAndClucV8XH.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/SMAOffsetProtectOptV1.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/SMAOffsetProtectOptV1_opt.py
-sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/user_data/strategies/CombinedBinHClucAndMADV9.py
