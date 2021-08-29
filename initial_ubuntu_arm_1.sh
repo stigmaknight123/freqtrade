d7b9cbf21e4eab7a25db7fb46ce4ceb309d45114
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2021-08-29 15:50:41 +0900

    update

--- initial_ubuntu_arm_1.sh
+++ initial_ubuntu_arm_1.sh
@@ -4,10 +4,57 @@
 sudo su
 apt update && apt -y install build-essential pkg-config libblosc-dev  libhdf5-dev libssl-dev
 
-# Install mambaforge (has most ARM ready packages, further details at https://github.com/conda-forge/miniforge )
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-pypy3-Linux-aarch64.sh
-bash Mambaforge-pypy3-Linux-aarch64.sh
-# just press enter a few times at license, and yes for default location
+# Install miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-aarch64.sh
+bash Miniconda3-py38_4.10.3-Linux-aarch64.sh
 
 # need to exit terminal and re-enter to activate base environment
-exit
+exit (or Ctrl+D)
+
+sudo su # check if (base) activated
+
+# install TA-Lib library first ( second half of https://mikestaszel.com/2021/01/23/install-ta-lib-on-m1/ for ARM linux)
+apt install -y build-essential automake
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+cp /usr/share/automake-1.16/config.guess .
+./configure --prefix=/usr
+make
+make install
+
+# install Freqtrade
+mkdir /freqtrade
+cd /freqtrade
+
+nano ftc.yml # paste contents of file that was sent separately
+conda install mamba -n base -c conda-forge
+mamba env create -n ftc -f ftc.yml
+conda activate ftc
+freqtrade -V
+freqtrade create-userdir --userdir user_data
+freqtrade install-ui
+
+# now freqtrade should be ready to run
+# ftc.yml is based on stable version, see notes in file if dev is preferred
+
+# suggest to set strategy inside the config file, then easy to swith strategy by editing config and reload_config. Easier for autostart, else need to reboot if want to swith strategy, see notes below.
+  "strategy": "MyStrategy"
+
+
+# for autostart at boot (did not test this on Oracle, but have used before on Ubuntu with Vultr)
+nano /freqtrade/autostart.sh
+
+# contents of autostart.sh
+cd /freqtrade && \
+source /root/miniconda3/etc/profile.d/conda.sh && \
+conda run --no-capture-output -n ftc freqtrade trade --logfile user_data/logs/freqtrade.log --db-url sqlite:////freqtrade/user_data/tradesv3.sqlite --config user_data/config.json
+
+# make executable
+chmod +x /freqtrade/autostart.sh
+
+# load at boat with cron
+crontab -e
+
+# add this line in cron file
+@reboot bash /freqtrade/autostart.sh
