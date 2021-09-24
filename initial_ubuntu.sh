4d3b26224be5685170a8c97ab82d8f7454877cb1
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2021-09-24 21:24:26 +0900

    Update initial_ubuntu.sh

--- initial_ubuntu.sh
+++ initial_ubuntu.sh
@@ -5,6 +5,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose python3-pip
+sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/swap_ubuntu_amd.sh
+bash swap_ubuntu_amd.sh
 mkdir ft_userdata
 sudo chown -R ubuntu:ubuntu ./ft_userdata
 sudo wget https://raw.githubusercontent.com/stash86/freqtrade/develop/initial_freqtrade.sh
