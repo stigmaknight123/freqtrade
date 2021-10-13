76a59792ff734d5a0a886fd84d3f12fb28da45fd
Stefano Ariestasia <stefano.ariestasia@gmail.com>
2021-10-14 08:15:17 +0900

    update list

--- user_data/backtest_results/get_blacklist.py
+++ user_data/backtest_results/get_blacklist.py
@@ -4,33 +4,28 @@ import os, sys
 file = ''
 
 with open(os.path.join(sys.path[0], '.last_result.json'), "r") as last_result_file:
-    last = json.load(last_result_file)
-    file = last['latest_backtest']
+  last = json.load(last_result_file)
+  file = last['latest_backtest']
 
 coins = []
 
 with open(os.path.join(sys.path[0], file), "r") as jsonfile:
   data = json.load(jsonfile)
-  for x in data:
-      if x == 'strategy':
-          y = data[x]
-          for z in y:
-              q = y[z]
-              for r in q:
-                  if r == 'results_per_pair':
-                      s = q[r]
-                      for t in s:
-                          if t['profit_mean'] < 0.08:
-                              string = t['key']
-                              coin = string.split("/", 1)
-                              #print(coin[0] + ' avg profit: ' + str(t['profit_mean']))
-                              coins.append(coin[0])
+  strategies = data["strategy"]
+  for strategy_name in strategies:
+    strategy = strategies[strategy_name]
+    results_per_pair = strategy["results_per_pair"]
+    for pair_result in results_per_pair:
+      if pair_result["profit_mean"] < 0.008:
+        pair = pair_result["key"]
+        coin = pair.split("/", 1)
+        coins.append(coin[0])
 
 coins.sort()
 coinstring = ''
 
 for c in coins:
-    coinstring += c + '|'
+  coinstring += c + '|'
 
 coinstring = coinstring[:-1]
 
